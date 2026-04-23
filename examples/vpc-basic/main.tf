3dee75c926e8107e708681c590d0c7dcc6b9cff8
vkharishb <v.harish.bodapati@gmail.com>
2026-04-23 21:55:08 +0530

    adding examples folder as a test env

---
+++ examples/vpc-basic/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "vpc" {
+  source = "../modules/vpc"
+
+  name = "demo-vpc"
+  cidr = "10.0.0.0/16"
+
+azs = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+}
