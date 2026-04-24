33e5a245d2edd7996346c7dc7401009951f22a03
vkharishb <v.harish.bodapati@gmail.com>
2026-04-24 23:19:29 +0530

    module path updated

--- examples/s3-basic/main.tf
+++ examples/s3-basic/main.tf
@@ -1,6 +1,6 @@
 module "s3" {
-  #source = "git::https://github.com/vkharishb/terraform-aws-modules.git//modules/s3?ref=main"
-  source = "../../modules/s3"
+  source = "git::https://github.com/vkharishb/terraform-aws-modules.git//modules/s3?ref=main"
+  #source = "../../modules/s3"
 
   bucket_name = "harish-demo-eks-state-12345"
 
