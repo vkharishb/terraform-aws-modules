0ba7e4ec22450135805409614e5b67d88e55f777
vkharishb <v.harish.bodapati@gmail.com>
2026-04-24 20:23:33 +0530

    Updated EC2 Module

--- modules/ec2/user_data/linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y amazon-cloudwatch-agent
-
-echo "Linux instance ready" > /home/ec2-user/status.txt
--- modules/ec2/user_data/windows.ps1
+++
@@ -1,3 +0,0 @@
-<powershell>
-Write-Output "Windows instance ready" > C:\status.txt
-</powershell>
