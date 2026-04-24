32a29296745e66e8617c15d0ce69e482e55baa72
vkharishb <v.harish.bodapati@gmail.com>
2026-04-24 20:27:41 +0530

    ec2 module adding and add test file

---
+++ examples/ec2-basic/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "ec2" {
+  source = "../../modules/ec2"
+
+  name           = "test-ec2"
+  ami_id         = data.aws_ami.amazon_linux.id
+  instance_type  = "t2.micro"
+  subnet_id      = data.aws_subnet.default.id
+
+  vpc_security_group_ids = [data.aws_security_group.default.id]
+
+  tags = {
+    Environment = "test"
+    Project     = "terraform-module-test"
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet" "default" {
+  default_for_az = true
+}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = data.aws_vpc.default.id
+}
