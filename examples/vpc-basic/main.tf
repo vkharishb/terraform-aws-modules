b0aac52e468bc62117f52f6c7454491c4fe6e1cb
vkharishb <v.harish.bodapati@gmail.com>
2026-04-24 20:45:02 +0530

    updating modules

--- examples/vpc-basic/main.tf
+++ examples/vpc-basic/main.tf
@@ -2,23 +2,33 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
 }
 
 module "vpc" {
-  source = "../modules/vpc"
+  source = "git::https://github.com/vkharishb/terraform-aws-modules.git//modules/vpc?ref=main"
 
   name = "demo-vpc"
   cidr = "10.0.0.0/16"
 
-azs = ["ap-south-1a", "ap-south-1b"]
+  azs = ["ap-south-1a", "ap-south-1b"]
+
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_flow_logs = false
+
+  tags = {
+    Environment = "dev"
+    Project     = "terraform-learning"
+  }
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_vpc" "this" {
   cidr_block           = var.cidr_block
   enable_dns_hostnames = true
